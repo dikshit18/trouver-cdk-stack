cb6fed9fa0f71c35272586ffc13050a521c80d9d
dikshit.kathuria <dikshit.kathuria@blazeclan.com>
2020-05-25 22:49:16 +0530

    Updated WebSocket stack, added dynamoDB

--- bin/cdk.js
+++ bin/cdk.js
@@ -2,7 +2,9 @@
 const cdk = require("@aws-cdk/core");
 const { CdkStack } = require("../lib/cdk-stack-admin");
 const { AdminStack } = require("../lib/admin-serverless-stack");
+const { WebSocketStack } = require("../lib/websocket-api-stack");
 
 const app = new cdk.App();
 //new CdkStack(app, "CdkStack");
-new AdminStack(app, "AdminStack");
+//new AdminStack(app, "AdminStack");
+new WebSocketStack(app, "WebSocketStack");
