608b52c2229d188072cbc833895de6c2dd98df81
dikshit.kathuria <dikshitkathuria1803@gmail.com>
2020-06-06 22:06:40 +0530

    Staff-serverless-stack pushed

--- bin/cdk.js
+++ bin/cdk.js
@@ -3,8 +3,10 @@ const cdk = require("@aws-cdk/core");
 const { CdkStack } = require("../lib/cdk-stack-admin");
 const { AdminStack } = require("../lib/admin-serverless-stack");
 const { WebSocketStack } = require("../lib/websocket-api-stack");
+const { StaffServiceStack } = require("../lib/staff-serverless-stack");
 
 const app = new cdk.App();
 //new CdkStack(app, "CdkStack");
 //new AdminStack(app, "AdminStack");
-new WebSocketStack(app, "WebSocketStack");
+//new WebSocketStack(app, "WebSocketStack");
+new StaffServiceStack(app, "StaffServiceStack");
