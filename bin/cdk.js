d921af3816dbdc5b0a36ba48ffd28ea0a6d6d48f
dikshit.kathuria <dikshitkathuria1803@gmail.com>
2020-06-08 19:53:58 +0530

    Configured staff websocket stack

--- bin/cdk.js
+++ bin/cdk.js
@@ -2,11 +2,13 @@
 const cdk = require("@aws-cdk/core");
 const { CdkStack } = require("../lib/cdk-stack-admin");
 const { AdminStack } = require("../lib/admin-serverless-stack");
-const { WebSocketStack } = require("../lib/websocket-api-stack");
+const { WebSocketStack } = require("../lib/admin-websocket-stack");
 const { StaffServiceStack } = require("../lib/staff-serverless-stack");
+const { StaffWebSocketStack } = require("../lib/staff-websocket-stack");
 
 const app = new cdk.App();
 //new CdkStack(app, "CdkStack");
 //new AdminStack(app, "AdminStack");
 //new WebSocketStack(app, "WebSocketStack");
-new StaffServiceStack(app, "StaffServiceStack");
+//new StaffServiceStack(app, "StaffServiceStack");
+new StaffWebSocketStack(app, "StaffWebSocketStack");
